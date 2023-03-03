cf2d2ec4cb23c82c5da5d5f24175371067e0e269
lesang.nguyen <nguyenlesang121999@gmail.com>
2023-03-03 16:44:51 +0700

    Add logic of FollowCamera

--- Assets/Script/FollowCamera.cs
+++ Assets/Script/FollowCamera.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+   [SerializeField] GameObject thingToFollow;
+     void LateUpdate() {
+        transform.position = thingToFollow.transform.position + new Vector3(0, 0, -10);
     }
 }
